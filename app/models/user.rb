9ebe9ceade22238cd3539af5d7cd969edc4ef70a
halezmarie <teacherhaleymarie@gmail.com>
2021-05-25 21:02:35 -0500

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@
 class User < ApplicationRecord
     has_secure_password
 
-
-
+    validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Must be a valid e-mail address!" }
+    # also adding it to the db as null:false adds extra protection 
 end
