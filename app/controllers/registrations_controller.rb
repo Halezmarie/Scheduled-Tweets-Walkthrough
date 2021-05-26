6a22a1ce0eba0eade1aea4ef1d579bbaedbfc7e9
halezmarie <teacherhaleymarie@gmail.com>
2021-05-25 23:03:38 -0500

    Added error messages when a user triessigning up with  the wrong format

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,25 @@
 class RegistrationsController < ApplicationController
     def new
-        #instance variable so that we can use it in our views
+        # instance variable so that we can use it in our views
         @user = User.new
+    end
+
+    def create
+       @user = User.new(user_params)
+       # take that hash of params and give it to user.new
+       if @user.save 
+        # if it is successful then redirect
+        redirect_to root_path, notice: "Successfully created account"
+       else
+        render :new
+    end
+end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+        # these are the only things you're allowed to set 
+
     end
 end
