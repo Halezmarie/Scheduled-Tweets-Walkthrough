6789a621337e173feee90ee27f4df58a9dbbefba
halezmarie <teacherhaleymarie@gmail.com>
2021-05-26 23:52:14 -0500

    Finished creating sign in feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,15 @@ class SessionsController < ApplicationController
 
 
 
-    def create
+    def create # sign in form 
+        user = User.find_by(email: params[:email])
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Logged in successfully"     
+        else
+            flash[:alert] = "Invalid email or password"
+            render :new
+        end
     end
 
     def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@
 # password_confirmation:string virtual
 
 class User < ApplicationRecord
-    has_secure_password
+    has_secure_password #gives validates, authentication, attributes 
 
     validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Must be a valid e-mail address!" }
     # also adding it to the db as null:false adds extra protection 
